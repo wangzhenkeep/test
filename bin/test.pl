9ea4314232c20757e37f5aacac4c73638898b31a
wangzhenkeep <wangzhen@celloud.cn>
2016-01-18 15:25:29 +0800

    delete

--- bin/test.pl
+++
@@ -1,3 +0,0 @@
-if (@ARGV!=1) {die"Usage: *.pl <projecyID>\n";exit;}
-$ARGV[0]=~s/ProjectID//;
-print "$ARGV[0]\n";
--- bin/wz_bin/test.sh
+++
@@ -1,8 +0,0 @@
-a=1
-b=1
-if (($a==1 && $b==2))
-then
-echo "yes"
-else
-echo $b
-fi
